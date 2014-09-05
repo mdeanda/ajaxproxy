37f0a1afe7a5da8249a2b06161e96ce17e9df3c3
miguel de anda <miguel@thedeanda.com>
2014-09-05 00:34:08 -0700

    jetty forced me to do a full implementation of HttpServletResponseWrapper that didn't exist HttpServletResponseWrapper for some reason... transparent proxy would unroll it otherwise. now proxied requests can be viewed in the logger again!

--- src/main/java/com/thedeanda/ajaxproxy/filter/MyServletResponseWrapper.java
+++ src/main/java/com/thedeanda/ajaxproxy/filter/MyServletResponseWrapper.java
@@ -4,25 +4,27 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Locale;
 
 import javax.servlet.ServletOutputStream;
-import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MyServletResponseWrapper extends HttpServletResponseWrapper {
+public class MyServletResponseWrapper implements HttpServletResponse {
 	private static final Logger log = LoggerFactory
 			.getLogger(MyServletResponseWrapper.class);
 	private ByteArrayOutputStream baos;
 	private PrintWriter writer;
 	private MyServletOutputStream os;
 	private int httpStatus = 200;
+	private HttpServletResponseWrapper impl;
 
 	public MyServletResponseWrapper(HttpServletResponse response) {
-		super(response);
+		impl = new HttpServletResponseWrapper(response);
 
 		baos = new ByteArrayOutputStream();
 		writer = new PrintWriter(baos);
@@ -30,8 +32,8 @@ public class MyServletResponseWrapper extends HttpServletResponseWrapper {
 	}
 
 	@Override
-	public void flushBuffer() {
-
+	public void flushBuffer() throws IOException {
+		impl.flushBuffer();
 	}
 
 	@Override
@@ -51,35 +53,155 @@ public class MyServletResponseWrapper extends HttpServletResponseWrapper {
 
 	@Override
 	public void reset() {
-		super.reset();
+		impl.reset();
 		this.httpStatus = SC_OK;
 	}
 
 	@Override
 	public void sendError(int sc) throws IOException {
 		httpStatus = sc;
-		super.sendError(sc);
+		impl.sendError(sc);
 	}
 
 	@Override
 	public void sendRedirect(String location) throws IOException {
 		httpStatus = 302;
-		super.sendRedirect(location);
+		impl.sendRedirect(location);
 	}
 
 	@Override
 	public void sendError(int sc, String msg) throws IOException {
 		httpStatus = sc;
-		super.sendError(sc, msg);
+		impl.sendError(sc, msg);
 	}
 
 	@Override
 	public void setStatus(int sc) {
 		httpStatus = sc;
-		super.setStatus(sc);
+		impl.setStatus(sc);
 	}
 
 	public int getStatus() {
 		return httpStatus;
+	}
+
+	@Override
+	public String getCharacterEncoding() {
+		return impl.getCharacterEncoding();
+	}
+
+	@Override
+	public String getContentType() {
+		return impl.getContentType();
+	}
+
+	@Override
+	public void setCharacterEncoding(String charset) {
+		impl.setCharacterEncoding(charset);
+	}
+
+	@Override
+	public void setContentLength(int len) {
+		impl.setContentLength(len);
+	}
+
+	@Override
+	public void setContentType(String type) {
+		impl.setContentType(type);
+	}
+
+	@Override
+	public void setBufferSize(int size) {
+		impl.setBufferSize(size);
+	}
+
+	@Override
+	public int getBufferSize() {
+		return impl.getBufferSize();
+	}
+
+	@Override
+	public void resetBuffer() {
+		impl.resetBuffer();
+	}
+
+	@Override
+	public boolean isCommitted() {
+		return impl.isCommitted();
+	}
+
+	@Override
+	public void setLocale(Locale loc) {
+		impl.setLocale(loc);
+	}
+
+	@Override
+	public Locale getLocale() {
+		return impl.getLocale();
+	}
+
+	@Override
+	public void addCookie(Cookie cookie) {
+		impl.addCookie(cookie);
+	}
+
+	@Override
+	public boolean containsHeader(String name) {
+		return impl.containsHeader(name);
+	}
+
+	@Override
+	public String encodeURL(String url) {
+		return impl.encodeUrl(url);
+	}
+
+	@Override
+	public String encodeRedirectURL(String url) {
+		return impl.encodeRedirectURL(url);
+	}
+
+	@Override
+	public String encodeUrl(String url) {
+		return impl.encodeUrl(url);
+	}
+
+	@Override
+	public String encodeRedirectUrl(String url) {
+		return impl.encodeRedirectUrl(url);
+	}
+
+	@Override
+	public void setDateHeader(String name, long date) {
+		impl.setDateHeader(name, date);
+	}
+
+	@Override
+	public void addDateHeader(String name, long date) {
+		impl.addDateHeader(name, date);
+	}
+
+	@Override
+	public void setHeader(String name, String value) {
+		impl.setHeader(name, value);
+	}
+
+	@Override
+	public void addHeader(String name, String value) {
+		impl.addHeader(name, value);
+	}
+
+	@Override
+	public void setIntHeader(String name, int value) {
+		impl.setIntHeader(name, value);
+	}
+
+	@Override
+	public void addIntHeader(String name, int value) {
+		impl.addIntHeader(name, value);
+	}
+
+	@Override
+	public void setStatus(int sc, String sm) {
+		impl.setStatus(sc, sm);
 	}
 }
