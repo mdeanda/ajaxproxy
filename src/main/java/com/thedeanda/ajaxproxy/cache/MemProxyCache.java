c32333c1061436f9484d8c8b5129c9374ac4c26c
miguel de anda <miguel@thedeanda.com>
2015-12-29 09:26:26 -0800

    basic cache implementation for testing

---
+++ src/main/java/com/thedeanda/ajaxproxy/cache/MemProxyCache.java
@@ -0,0 +1,50 @@
+package com.thedeanda.ajaxproxy.cache;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.thedeanda.ajaxproxy.cache.model.CachedResponse;
+
+/** in memory implementation of cache */
+public class MemProxyCache extends LinkedHashMap<String, CachedResponse>
+		implements ProxyCache {
+	private static final long serialVersionUID = -5854542957026651145L;
+	private static final Logger log = LoggerFactory
+			.getLogger(MemProxyCache.class);
+	private static final int capacity = 100;
+
+	private Object lock = new Object();
+
+	public MemProxyCache() {
+		super(capacity, 1.1f, true);
+	}
+
+	@Override
+	public void clearCache() {
+		synchronized (lock) {
+			clear();
+		}
+	}
+
+	@Override
+	public void cache(CachedResponse response) {
+		synchronized (lock) {
+			put(response.getUrl(), response);
+		}
+	}
+
+	@Override
+	public CachedResponse get(String urlPath) {
+		synchronized (lock) {
+			return get(urlPath);
+		}
+	}
+
+	@Override
+	protected boolean removeEldestEntry(Map.Entry<String, CachedResponse> eldest) {
+		return size() >= capacity;
+	}
+}
