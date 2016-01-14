c742456b0dfe1929623ff8df949032a9c421913c
miguel de anda <miguel@thedeanda.com>
2016-01-13 20:08:32 -0800

    small text field enhancements

--- src/main/java/com/thedeanda/ajaxproxy/ui/proxy/ProxyEditorPanel.java
+++ src/main/java/com/thedeanda/ajaxproxy/ui/proxy/ProxyEditorPanel.java
@@ -5,6 +5,10 @@ import java.awt.Dimension;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -41,16 +45,19 @@ public class ProxyEditorPanel extends JPanel {
 
 		domainLabel = new JLabel("Host");
 		hostField = SwingUtils.newJTextField();
+		addListeners(hostField);
 		add(domainLabel);
 		add(hostField);
 
 		portLabel = new JLabel("Port");
 		portField = SwingUtils.newJTextField();
+		addListeners(portField);
 		add(portLabel);
 		add(portField);
 
 		pathLabel = new JLabel("Path");
 		pathField = SwingUtils.newJTextField();
+		addListeners(pathField);
 		add(pathLabel);
 		add(pathField);
 
@@ -78,6 +85,41 @@ public class ProxyEditorPanel extends JPanel {
 		initLayout();
 		setPreferredSize(new Dimension(700, 80));
 		setMinimumSize(new Dimension(500, 80));
+	}
+
+	private void addListeners(final JTextField txtField) {
+		txtField.addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					commitEdit();
+				}
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		txtField.addFocusListener(new FocusListener() {
+
+			@Override
+			public void focusGained(FocusEvent e) {
+				txtField.select(0, txtField.getText().length());
+			}
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				txtField.select(0, 0);
+			}
+		});
 	}
 
 	private void initNewProxyListener() {
