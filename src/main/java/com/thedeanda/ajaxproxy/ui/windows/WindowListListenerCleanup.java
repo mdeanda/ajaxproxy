44cc9542e11bba2cd48014dcfcdf852546456372
mdeanda <miguel@thedeanda.com>
2016-05-09 23:00:04 -0700

    remove generated comments

--- src/main/java/com/thedeanda/ajaxproxy/ui/windows/WindowListListenerCleanup.java
+++ src/main/java/com/thedeanda/ajaxproxy/ui/windows/WindowListListenerCleanup.java
@@ -13,8 +13,6 @@ public class WindowListListenerCleanup implements WindowListener {
 	
 	@Override
 	public void windowOpened(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
@@ -24,32 +22,22 @@ public class WindowListListenerCleanup implements WindowListener {
 
 	@Override
 	public void windowClosed(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void windowIconified(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void windowDeiconified(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void windowActivated(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void windowDeactivated(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
