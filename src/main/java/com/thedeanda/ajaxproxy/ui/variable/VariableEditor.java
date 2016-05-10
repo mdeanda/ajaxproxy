c8927afe23c7451959d3bd065d4965f4553da4e3
mdeanda <miguel@thedeanda.com>
2016-05-10 01:51:27 -0700

    variable panel

---
+++ src/main/java/com/thedeanda/ajaxproxy/ui/variable/VariableEditor.java
@@ -0,0 +1,110 @@
+package com.thedeanda.ajaxproxy.ui.variable;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+import com.thedeanda.ajaxproxy.ui.SwingUtils;
+
+public class VariableEditor extends JPanel {
+	private SpringLayout layout;
+	private JLabel keyLabel;
+	private JTextField keyField;
+	private JLabel valueLabel;
+	private JTextField valueField;
+	private JButton btn;
+
+	public VariableEditor() {
+		layout = new SpringLayout();
+		setLayout(layout);
+
+		
+		keyLabel = new JLabel("Key");
+		keyField = SwingUtils.newJTextField();
+		add(keyLabel);
+		add(keyField);
+		
+		valueLabel = new JLabel("Value");
+		valueField = SwingUtils.newJTextField();
+		add(valueLabel);
+		add(valueField);
+		
+		
+		btn = new JButton("Ok");
+		btn.setMargin(new Insets(2, 14, 2, 14));
+		btn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				commitEdit();
+			}
+		});
+		add(btn);
+
+
+		initLayout();
+		
+	}
+	
+
+	private void commitEdit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initLayout() {
+		JLabel[] labels = new JLabel[] { keyLabel, valueLabel };
+		Component[] fields = new Component[] { keyField, valueField };
+		int[] cols = new int[] { 350, 350 };
+
+		for (int i = cols.length - 1; i >= 0; i--) {
+			JLabel lbl = labels[i];
+			Component fld = fields[i];
+			if (i == 0) {
+				layout.putConstraint(SpringLayout.NORTH, lbl, 0,
+						SpringLayout.NORTH, this);
+
+				layout.putConstraint(SpringLayout.NORTH, fld, 2,
+						SpringLayout.SOUTH, lbl);
+
+				layout.putConstraint(SpringLayout.WEST, fld, 0,
+						SpringLayout.WEST, this);
+
+				layout.putConstraint(SpringLayout.WEST, lbl, 0,
+						SpringLayout.WEST, this);
+			} else {
+				layout.putConstraint(SpringLayout.BASELINE, fld, 0,
+						SpringLayout.BASELINE, fields[0]);
+
+				layout.putConstraint(SpringLayout.WEST, fld, 10,
+						SpringLayout.EAST, fields[i - 1]);
+				if (lbl != null) {
+					layout.putConstraint(SpringLayout.VERTICAL_CENTER, lbl, 0,
+							SpringLayout.VERTICAL_CENTER, labels[0]);
+					layout.putConstraint(SpringLayout.WEST, lbl, 0,
+							SpringLayout.WEST, fld);
+				}
+			}
+			if (i == cols.length - 1) {
+				layout.putConstraint(SpringLayout.EAST, fld, -10,
+						SpringLayout.WEST, btn);
+			} else {
+				layout.putConstraint(SpringLayout.EAST, fld, cols[i],
+						SpringLayout.WEST, fld);
+			}
+
+		}
+
+		layout.putConstraint(SpringLayout.BASELINE, btn, 0,
+				SpringLayout.BASELINE, keyField);
+		layout.putConstraint(SpringLayout.EAST, btn, 0, SpringLayout.EAST, this);
+
+	}
+}
--- src/main/java/com/thedeanda/ajaxproxy/ui/variable/VariablesPanel.java
+++ src/main/java/com/thedeanda/ajaxproxy/ui/variable/VariablesPanel.java
@@ -11,9 +11,9 @@ import com.thedeanda.ajaxproxy.ui.SettingsChangedListener;
 
 public class VariablesPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
+	private VariableEditor variableEditor;
 
-	public VariablesPanel(final SettingsChangedListener listener,
-			VariableTableModel variableModel) {
+	public VariablesPanel(final SettingsChangedListener listener, VariableTableModel variableModel) {
 		SpringLayout layout = new SpringLayout();
 		setLayout(layout);
 
@@ -29,15 +29,18 @@ public class VariablesPanel extends JPanel {
 			}
 		});
 
-		layout.putConstraint(SpringLayout.NORTH, scroll, 10,
-				SpringLayout.NORTH, this);
-		layout.putConstraint(SpringLayout.WEST, scroll, 10, SpringLayout.WEST,
-				this);
-		layout.putConstraint(SpringLayout.EAST, scroll, -10, SpringLayout.EAST,
-				this);
-		layout.putConstraint(SpringLayout.SOUTH, scroll, -10,
-				SpringLayout.SOUTH, this);
+		variableEditor = new VariableEditor();
+		add(variableEditor);
 
+		layout.putConstraint(SpringLayout.NORTH, scroll, 10, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.WEST, scroll, 10, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.EAST, scroll, -10, SpringLayout.EAST, this);
+		layout.putConstraint(SpringLayout.SOUTH, scroll, -10, SpringLayout.NORTH, variableEditor);
+
+		layout.putConstraint(SpringLayout.WEST, variableEditor, 10, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.EAST, variableEditor, -10, SpringLayout.EAST, this);
+		layout.putConstraint(SpringLayout.SOUTH, variableEditor, -10, SpringLayout.SOUTH, this);
+		layout.putConstraint(SpringLayout.NORTH, variableEditor, -90, SpringLayout.SOUTH, this);
 	}
 
 }
