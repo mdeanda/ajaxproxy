25034ceb0b86197d4d2614daa75f0c7151038bc0
miguel de anda <miguel@thedeanda.com>
2018-05-16 23:45:10 -0700

    notes

--- src/main/java/com/thedeanda/ajaxproxy/model/config/Variable.java
+++ src/main/java/com/thedeanda/ajaxproxy/model/config/Variable.java
@@ -2,6 +2,7 @@ package com.thedeanda.ajaxproxy.model.config;
 
 import lombok.Data;
 
+//NOTE: only used as ui dto, move to ui.variable.model package
 @Data
 public class Variable {
 	private String key;
