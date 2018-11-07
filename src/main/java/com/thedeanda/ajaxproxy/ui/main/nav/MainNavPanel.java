0cc78dbe7e2653949403162ef2bb554a1b65c7f9
Miguel De Anda <miguel@thedeanda.com>
2018-11-06 22:45:05 -0800

    updating panel buttons, not functional yet

--- src/main/java/com/thedeanda/ajaxproxy/ui/main/nav/MainNavPanel.java
+++ src/main/java/com/thedeanda/ajaxproxy/ui/main/nav/MainNavPanel.java
@@ -1,25 +1,23 @@
 package com.thedeanda.ajaxproxy.ui.main.nav;
 
-import java.awt.Dimension;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JPanel;
-import javax.swing.JToggleButton;
-import javax.swing.SpringLayout;
+import javax.swing.*;
 
+import com.thedeanda.ajaxproxy.ui.border.BottomBorder;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class MainNavPanel extends JPanel {
 	private static final long serialVersionUID = -844239892839389886L;
-	private List<JToggleButton> buttons = new ArrayList<>();
+	private List<JComponent> buttons = new ArrayList<>();
 
 	private Set<NavListener> listeners = new HashSet<>();
 
@@ -85,21 +83,100 @@ public class MainNavPanel extends JPanel {
 
 	private JComponent createNavButton(JPanel pnl, String label, final String tooltip, final NavItem navItem,
 			final int index) {
-		final JToggleButton btn = new JToggleButton(label);
-		// btn.setPreferredSize(btn.getMaximumSize());
-		buttons.add(btn);
-		btn.setToolTipText(tooltip);
-		pnl.add(btn);
+		JComponent newButton = null;
+		if (navItem == NavItem.Server) {
+			newButton = createServerButton(pnl, label, tooltip, navItem, index);
+		} else {
+			final JToggleButton btn = new JToggleButton(label);
+			btn.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					deselectAll(btn);
+					fireSelected(navItem, index);
+				}
+			});
+			btn.setToolTipText(tooltip);
+			newButton = btn;
+		}
+		buttons.add(newButton);
+		pnl.add(newButton);
+		return newButton;
+	}
 
-		btn.addActionListener(new ActionListener() {
+	private JComponent createServerButton(JPanel pnl, String label, final String tooltip, final NavItem navItem,
+										  final int index) {
+		JPanel panel = new JPanel();
+		SpringLayout layout = new SpringLayout();
+		panel.setLayout(layout);
+		panel.setBorder(new BottomBorder());
+
+
+		JLabel lbl = new JLabel(label);
+		lbl.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lbl);
+
+		JToolBar toolbar = new JToolBar("Toolbar");
+		toolbar.setFloatable(false);
+		toolbar.setBorderPainted(false);
+		add(toolbar);
+
+
+		JButton editButton = makeNavigationButton("server_edit.png", "EDIT", "Server Settings");
+		/*
+		view.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				deselectAll(btn);
+				deselectAll(view);
 				fireSelected(navItem, index);
 			}
 		});
+		*/
+		toolbar.add(editButton);
 
-		return btn;
+		JButton editButton2 = makeNavigationButton("control_play.png", "RUN", "Start Server");
+		toolbar.add(editButton2);
+		JButton editButton3 = makeNavigationButton("control_stop.png", "STOP", "Stop Server");
+		toolbar.add(editButton3);
+		editButton3.setEnabled(false);
+
+
+
+		panel.add(toolbar);
+
+
+		layout.putConstraint(SpringLayout.NORTH, lbl, 0, SpringLayout.NORTH, panel);
+		layout.putConstraint(SpringLayout.WEST, lbl, 0, SpringLayout.WEST, panel);
+		layout.putConstraint(SpringLayout.EAST, lbl, 0, SpringLayout.EAST, panel);
+		layout.putConstraint(SpringLayout.SOUTH, lbl, 25, SpringLayout.NORTH, lbl);
+
+		layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, toolbar, 0, SpringLayout.HORIZONTAL_CENTER, panel);
+		//layout.putConstraint(SpringLayout.WEST, editButton, 0, SpringLayout.WEST, panel);
+		//layout.putConstraint(SpringLayout.EAST, editButton, 0, SpringLayout.EAST, panel);
+		layout.putConstraint(SpringLayout.SOUTH, toolbar, 0, SpringLayout.SOUTH, panel);
+
+		panel.setPreferredSize(new Dimension(100, 60));
+		return panel;
+	}
+
+	protected JButton makeNavigationButton(String imageName, String actionCommand, String toolTipText) {
+		// Look for the image.
+		String imgLocation = "/images/" + imageName;
+		URL imageURL = getClass().getResource(imgLocation);
+
+		// Create and initialize the button.
+		JButton button = new JButton();
+		button.setActionCommand(actionCommand);
+		button.setToolTipText(toolTipText);
+		//button.addActionListener(this);
+
+		if (imageURL != null) { // image found
+			button.setIcon(new ImageIcon(imageURL, toolTipText));
+		} else { // no image found
+			button.setText(toolTipText);
+			log.warn("Resource not found: {}", imgLocation);
+		}
+
+		return button;
 	}
 
 	private void fireSelected(NavItem navItem, int index) {
@@ -110,11 +187,13 @@ public class MainNavPanel extends JPanel {
 	}
 
 	private void deselectAll(JToggleButton keepSelected) {
+		/*
 		for (JToggleButton btn : buttons) {
 			if (btn != keepSelected) {
 				btn.setSelected(false);
 			}
 		}
+		//*/
 	}
 
 	private void resetLayout(JPanel panel) {
@@ -130,8 +209,8 @@ public class MainNavPanel extends JPanel {
 			}
 			// layout.putConstraint(SpringLayout.SOUTH, btn, 0, SpringLayout.SOUTH, panel);
 
-			layout.putConstraint(SpringLayout.WEST, btn, 5, SpringLayout.WEST, panel);
-			layout.putConstraint(SpringLayout.EAST, btn, -8, SpringLayout.EAST, panel);
+			layout.putConstraint(SpringLayout.WEST, btn, 0, SpringLayout.WEST, panel);
+			layout.putConstraint(SpringLayout.EAST, btn, -3, SpringLayout.EAST, panel);
 
 			lastButton = btn;
 		}
