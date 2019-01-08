5d1c79b99f6eebf6ae652737399c7154ab49ebaf
Miguel De Anda <miguel@thedeanda.com>
2019-01-07 21:56:40 -0800

    trying to fix ormlite

--- src/main/java/com/thedeanda/ajaxproxy/service/StoredResource.java
+++ src/main/java/com/thedeanda/ajaxproxy/service/StoredResource.java
@@ -3,6 +3,7 @@ package com.thedeanda.ajaxproxy.service;
 import com.j256.ormlite.field.DataType;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
+import lombok.Data;
 
 /**
  * TODO: move this to "model" package
@@ -10,6 +11,7 @@ import com.j256.ormlite.table.DatabaseTable;
  *
  */
 @DatabaseTable(tableName = "resource")
+@Data
 public class StoredResource {
 	@DatabaseField(id = true, width = 64)
 	private String id;
@@ -53,117 +55,5 @@ public class StoredResource {
 	private String contentEncoding;
 	
 	private byte[] outputDecompressed;
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	public String getMethod() {
-		return method;
-	}
-
-	public void setMethod(String method) {
-		this.method = method;
-	}
-
-	public String getHeaders() {
-		return headers;
-	}
-
-	public void setHeaders(String headers) {
-		this.headers = headers;
-	}
-
-	public byte[] getInput() {
-		return input;
-	}
-
-	public void setInput(byte[] input) {
-		this.input = input;
-	}
-
-	public int getStatus() {
-		return status;
-	}
-
-	public void setStatus(int status) {
-		this.status = status;
-	}
-
-	public String getReason() {
-		return reason;
-	}
-
-	public void setReason(String reason) {
-		this.reason = reason;
-	}
-
-	public long getDuration() {
-		return duration;
-	}
-
-	public void setDuration(long duration) {
-		this.duration = duration;
-	}
-
-	public String getResponseHeaders() {
-		return responseHeaders;
-	}
-
-	public void setResponseHeaders(String responseHeaders) {
-		this.responseHeaders = responseHeaders;
-	}
-
-	public byte[] getOutput() {
-		return output;
-	}
-
-	public void setOutput(byte[] output) {
-		this.output = output;
-	}
-
-	public String getErrorMessage() {
-		return errorMessage;
-	}
-
-	public void setErrorMessage(String errorMessage) {
-		this.errorMessage = errorMessage;
-	}
-
-	public long getStartTime() {
-		return startTime;
-	}
-
-	public void setStartTime(long startTime) {
-		this.startTime = startTime;
-	}
-
-	public String getContentEncoding() {
-		return contentEncoding;
-	}
-
-	public void setContentEncoding(String contentEncoding) {
-		this.contentEncoding = contentEncoding;
-	}
-
-	public byte[] getOutputDecompressed() {
-		return outputDecompressed;
-	}
-
-	public void setOutputDecompressed(byte[] outputDecompressed) {
-		this.outputDecompressed = outputDecompressed;
-	}
 
 }
