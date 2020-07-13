36460f6c4d6328a0c0dedfdb6a05eac7c8c3a582
Miguel De Anda <miguel@thedeanda.com>
2020-07-13 06:33:33 -0700

    move resource path

--- src/main/java/com/thedeanda/ajaxproxy/resources/VariableResource.java
+++ src/main/java/com/thedeanda/ajaxproxy/resources/VariableResource.java
@@ -10,7 +10,7 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Slf4j
-@Path("/variable")
+@Path("/config/variable")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class VariableResource {
